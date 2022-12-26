059204ce1197e204e4b0d2fe4a5b31ac21198d97
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-12-26 16:11:45 +0600

    Fontend : home page- top banner section- name type animation added

--- frontend-react-app/src/components/TopBanner/TopBanner.js
+++ frontend-react-app/src/components/TopBanner/TopBanner.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from "react";
 import { Row, Col, Container, Button } from "react-bootstrap";
+import { TypeAnimation } from "react-type-animation";
 
 class TopBanner extends Component{
     render(){
@@ -10,8 +11,22 @@ class TopBanner extends Component{
                         <Container className="topBannerContent">
                             <Row>
                                 <Col className="text-center">
-                                    <h1 className="topBannerTitle">Hi, I am <span style={{color:'#ffa731'}}>Mamunur Rashid</span></h1>
-                                    <h4 className="topBannerSubTitle">Software Engineer</h4>
+                                    <h1 className="topBannerTitle">Hi, I am <span style={{color:'#ffa731'}}>Mamunur Rashid</span>
+                                    </h1>
+                                    
+                                    <h4 className="topBannerSubTitle">
+                                        <TypeAnimation
+                                                // Same String at the start will only be typed once, initially
+                                                sequence={[
+                                                'Software Engineer', 1000,
+                                                'Full Stack Web Developer', 1000,
+                                                'API Developer', 1000,
+                                                ]}
+                                                speed={1} // Custom Speed from 1-99 - Default Speed: 40
+                                                wrapper="span" // Animation will be rendered as a <span>
+                                                repeat={Infinity} // Repeat this Animation Sequence infinitely
+                                        />
+                                    </h4>
                                     <Button variant="warning">More Info</Button>
                                 </Col>
                             </Row>
