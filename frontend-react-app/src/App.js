5e25629a8e6384f9b06efa1d94a1e98da728c8aa
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-12-28 10:34:51 +0600

    FrontEnd : Home page section formation completed

--- frontend-react-app/src/App.js
+++ frontend-react-app/src/App.js
@@ -2,30 +2,12 @@ import logo from './logo.svg';
 // import './App.css';
 import '../src/asset/css/bootstrap.min.css'
 import '../src/asset/css/custom.css';
-import TopBanner from './components/TopBanner/TopBanner';
-import Services from './components/Services/Services';
-import TopNavigation from './components/TopNavigation/TopNavigation';
-import Analysis from './components/Analysis/Analysis';
-import Summary from './components/Summary/Summary';
-import Projects from './components/Projects/Projects';
-import Courses from './components/Courses/Courses';
-import Video from './components/Video/Video';
-import ClientReview from './components/ClientReview/ClientReview';
-import Footer from './components/Footer/Footer';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
-        <TopNavigation/>
-        <TopBanner/>
-        <Services/>
-        <Analysis/>
-        <Summary/>
-        <Projects/>
-        <Courses/>
-        <Video/>
-        <ClientReview/>
-        <Footer/>
+        <HomePage/>
     </div>
   );
 }
