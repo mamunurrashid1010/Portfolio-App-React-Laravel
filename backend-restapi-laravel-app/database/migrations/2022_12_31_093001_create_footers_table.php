1edb0acc0e7d8e97fb23fa3ce0b590ea9cb51708
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-12-31 15:42:48 +0600

    BackEnd-RestApi : footers table schema, model, controller create.

---
+++ backend-restapi-laravel-app/database/migrations/2022_12_31_093001_create_footers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFootersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('footers', function (Blueprint $table) {
+            $table->id();
+            $table->text('address')->nullable(true);
+            $table->string('email')->nullable(true);
+            $table->string('phone')->nullable(true);
+            $table->string('facebook_link')->nullable(true);
+            $table->string('youtube_link')->nullable(true);
+            $table->string('footer_credit')->nullable(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('footers');
+    }
+}
