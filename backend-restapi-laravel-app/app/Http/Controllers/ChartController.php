7a35904304609445209971ed5bd8d728c44cf715
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2023-01-01 11:20:17 +0600

    BackEnd-RestApi : Technologies section- RestApi create for get all technologies.

--- backend-restapi-laravel-app/app/Http/Controllers/ChartController.php
+++ backend-restapi-laravel-app/app/Http/Controllers/ChartController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Chart;
 use Illuminate\Http\Request;
 
 class ChartController extends Controller
 {
-    //
+    /*
+     * getAllTechnologies method
+     * return technologies list
+     */
+    public function getAllTechnologies(){
+        $technologies = Chart::select('id','x_data','y_data')->get();
+        return $technologies;
+    }
 }
