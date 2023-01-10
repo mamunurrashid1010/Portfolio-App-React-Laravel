ce4f51c9db115e83ee2a7dc56258113064cf0d3f
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2023-01-10 10:46:47 +0600

    FrontEnd : loading component create.

---
+++ frontend-react-app/src/components/Loading/Loading.js
@@ -0,0 +1,21 @@
+import React, { Component, Fragment } from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import loadingImage from '../../asset/image/loader';
+
+class Loading extends Component {
+    render() {
+        return (
+            <Fragment>
+                <Container className='text-center'>
+                    <Row>
+                        <Col>
+                            <img className='loaderAnimation' src={loadingImage} />
+                        </Col>
+                    </Row>
+                </Container>
+            </Fragment>
+        );
+    }
+}
+
+export default Loading;
