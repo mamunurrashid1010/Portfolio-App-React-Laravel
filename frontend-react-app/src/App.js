18f8a599320837708eaf7185e87fb73e758e7f09
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2023-01-11 11:48:04 +0600

    FrontEnd : live server app url not found, error solved using HashRouter.

--- frontend-react-app/src/App.js
+++ frontend-react-app/src/App.js
@@ -2,15 +2,15 @@ import logo from './logo.svg';
 // import './App.css';
 import '../src/asset/css/bootstrap.min.css'
 import '../src/asset/css/custom.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import AppRouter from './route/AppRouter';
 
 function App() {
   return (
     <div className="App">
-        <BrowserRouter>
+        <HashRouter>
         <AppRouter/>
-        </BrowserRouter>
+        </HashRouter>
     </div>
   );
 }
